c60631188bed33f3db7b12e9f4a636367e747cee
pablugj <pablo.ulises.gnz@tecsanpedro.edu.mx>
2017-06-01 00:48:32 -0500

    mas

--- js/main.js
+++ js/main.js
@@ -2,24 +2,28 @@ $(inicio);
 
 function inicio()
 {
-	$('#home').on('click',cargar);
+	//$('#home').on('click',cargar);
+	$("#menu > li > a").on('click',cargar);
 }
 
 function cargar()
 {
 	$('.contentsection1').html("<img id='loader_gif' alt='' src='img/loading_apple.gif' width='100px'>   ");
 	//$('.contentsection1').html("inicio.html");
-	var id = "inicio.html";
-	setTimeout(termina,1000,id);
+	var url = $(this).attr("id")+".html"
+	console.log(url);
+	//var id = "inicio.html";
+	//setTimeout(termina,1000,id);
 }
 
 function termina(id)
 {
-	//console.log(id);
-	$.get(id, function(htmlexterno) 
+	console.log(id);
+	$.get(id,function (htmlexterno)
 	{
 		console.log(htmlexterno);
-	   $('.contentsection1').html(htmlexterno);
+		$('.contentsection1').html(htmlexterno);
+
 	});
 	
 }
